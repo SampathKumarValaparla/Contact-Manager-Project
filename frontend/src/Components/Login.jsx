c33b8855dad5d032520daf77a3ed172888fb9f9f
Him1998 <57448709+Him1998@users.noreply.github.com>
2023-03-03 08:05:49 +0530

    Update Login.jsx

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -24,7 +24,7 @@ function Login() {
       console.log(val,key);
     })
 
-    const res = await fetch('http://localhost:8080/user/',
+    const res = await fetch('http://localhost:8080/user/login',
     {
       method : "POST",
       body : formData
